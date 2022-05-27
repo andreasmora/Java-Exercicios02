5b3742e99df99b4b4bead049a29fe05b96c24d37
Andreas Mora75 <70494818+andreasmora@users.noreply.github.com>
2022-05-27 12:40:14 -0300

    Add files via upload

---
+++ Ex060.java
@@ -0,0 +1,32 @@
+// Faça  um  programa  que  calcule  o  mostre  a  média
+// aritmética  de  N notas.
+
+import java.util.Scanner;
+
+public class Ex060 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Entre com a quantidade de notas: ");
+        int notas = scan.nextInt();
+
+        double soma = 0;
+        double media;
+        double nota;
+
+        for (int i=0; i<notas; i++){
+            System.out.println("Entre com a nota: " + (i+1));
+            nota = scan.nextDouble();
+            soma += nota;
+
+        }
+
+        media = soma / notas;
+        System.out.println("Soma: " + soma);
+        System.out.println("Media: " + media);
+
+
+
+    }
+}
